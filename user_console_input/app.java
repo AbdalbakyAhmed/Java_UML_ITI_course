ecf0f02a985d7d3825ae920fb360730c4ca5dd8b
Ahmed Rabie <abdalbaky.ahmed@gmail.com>
2021-05-26 13:30:41 +0200

    update condition scope

--- user_console_input/app.java
+++ user_console_input/app.java
@@ -6,31 +6,29 @@ public class app {
     public static void main(String[] args)
     {
         // Using Console to input data from user
-        String _str = System.console().readLine();
+        String _str ;
         String stop = "stop";
         List<String> list = new ArrayList<String>();
         // 
-        if (! _str.equalsIgnoreCase(stop)) {
-            list.add(_str);
-        }
-        System.out.println("You entered : " + _str);
-        
-        while (! _str.equalsIgnoreCase(stop)){
+        do {
             // Using Console to input data from user
             _str = System.console().readLine();
+            System.out.println("You entered string " + _str);  
             if (! _str.equalsIgnoreCase(stop)) {
                 list.add(_str);
-            }
-            System.out.println("You entered string " + _str);
-        }
+            }          
+        } while (! _str.equalsIgnoreCase(stop));
         System.out.println(list);
+        
+        // 
         try {
             FileWriter writer = new FileWriter("output.txt");
             for (String string : list) {
                 writer.write(string+"\n");
             }
             writer.close();
-        } catch (IOException e) {
+        } 
+        catch (IOException e) {
             e.printStackTrace();
         } 
     }
